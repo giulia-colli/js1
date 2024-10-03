005283852f155ba7e845f3cb9690fd837cad8316
giulia-colli <giulia.colli.social@gmail.com>
2024-10-03 12:09:36 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,69 @@
+/*
+REGOLE
+- Tutte le risposte devono essere scritte in JavaScript
+- Se sei in difficoltà puoi chiedere aiuto a un Teaching Assistant
+- Puoi usare Google / StackOverflow ma solo quanto ritieni di aver bisogno di qualcosa che non è stato spiegato a lezione
+- Puoi testare il tuo codice in un file separato, o de-commentando un esercizio alla volta
+- Per farlo puoi utilizzare il terminale Bash, quello di VSCode o quello del tuo sistema operativo (se utilizzi macOS o Linux)
+*/
+
+/* ESERCIZIO 1
+ Elenca e descrivi i principali datatype in JavaScript. Prova a spiegarli come se volessi farli comprendere a un bambino.
+*/
+
+/* Numeri: per fare conti.
+Stringhe :per scrivere parole.
+Booleani :per rispondere a domande con "sì" o "no".
+Null :per indicare qualcosa che è vuoto.
+Undefined :quando non sappiamo ancora cosa c'è.
+Oggetti: per descrivere cose complesse.
+Array :per fare liste.
+Simboli: per identificare cose uniche.
+BigInt: per numeri molto grandi!*/
+
+/* ESERCIZIO 2
+ Descrivi cos'è un oggetto in JavaScript, con parole tue.
+*/
+
+/* è un contenitore che può avere al suo interno tanti tipi di dati, può ad esempio essere utile quando devi descrivere cose complesse*/
+
+/* ESERCIZIO 3
+ Scriti il codice necessario ad effettuare un addizione (una somma) dei numeri 12 e 20.
+*/
+/*let num1 = 12
+let num2 = 20
+let somma = num1 + num2
+console.log ("la somma di 12 e 20 è:"+somma)*/
+
+/* ESERCIZIO 4
+ Crea una variable di nome "x" e assegna ad essa il numero 12.
+*/
+
+/* let x = 12 */
+
+/* ESERCIZIO 5
+ Crea una variable chiamata "name" e assegna ad essa il tuo nome, sotto forma di stringa.
+*/
+
+/* let name = giulia */
+
+/* ESERCIZIO 6
+ Esegui una sottrazione tra i numeri 4 e la variable "x" appena dichiarata (che contiene il numero 12).
+*/
+
+/*let numero4 = 4
+let x =12
+let sottrazione = numero4 - x
+console.log ("la sottrazione di 4 - 12:" + sottrazione) */
+
+/* ESERCIZIO 7
+ Crea due variabili: "name1" e "name2". Assegna a name1 la stringa "john", e assegna a name2 la stringa "John" (con la J maiuscola!).
+ Verifica che name1 sia diversa da name2 (suggerimento: è la stessa cosa di verificare che la loro uguaglianza sia falsa).
+ Infine, verifica che la loro uguaglianza diventi true se entrambe vengono trasformate in lowercase (senza cambiare il valore di name2!).
+ NON HAI BISOGNO DI UN BLOCCO IF/ELSE. E' sufficiente utilizzare console.log().
+*/
+/*let name1 = "John"
+let name2 = "john"
+let sonoDiversi = name1 !== name2
+console.log("name1 è diverso da name2:" +sonoDiversi)
+*/
